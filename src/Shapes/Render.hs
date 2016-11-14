b2e2f2faf4250fc1e24581d9e9007fd7fb279c5d
Niall Paterson <npaters@tcd.ie>
2016-11-14 12:45:51 +0000

    Update cabal file, add stylesheet

---
+++ src/Shapes/Render.hs
@@ -0,0 +1,16 @@
+module Shapes.Render where
+
+import Shapes.Shape
+
+import Text.Blaze.Svg11 ((!), mkPath, rotate, l, m)
+import qualified Text.Blaze.Svg11 as S
+import qualified Text.Blaze.Svg11.Attributes as A
+
+svgHeader = S.docTypeSvg ! A.version "1.1" ! A.width "150" ! A.height "100" ! A.viewbox "0 0 3 2"
+
+svgTemplate shapeSvg = svgHeader $ do
+    S.g shapeSvg
+
+toSvg :: Drawing -> S.Svg
+toSvg [(_, shape)] = svgTemplate $ do
+    S.rect ! A.width "1" ! A.height "1" ! A.fill "#03"
